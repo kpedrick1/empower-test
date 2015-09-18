fab27eedc9c429c81f560b96d2066b4b5a8dc1ea
ebtalley <eric@leadcliq.com>
2015-09-17 23:46:43 -0700

    remove bpcare from footer and logout path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -63,7 +63,7 @@ class ApplicationController < ActionController::Base
   # Overwriting the sign_out redirect path method
   def after_sign_out_path_for(resource)
     #root_path
-    'https://www.puracap.com/'
+    'http://www.puracap.com/'
   end
     
 end
