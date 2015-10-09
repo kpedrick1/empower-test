ac6c462c082312bd6152a0c2729a76e21ec29bd0
ebtalley <eric@leadcliq.com>
2015-10-09 10:26:14 -0700

    place order page

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_puracap-qa_session'
+Rails.application.config.session_store :cookie_store, key: '_puracap_session'
