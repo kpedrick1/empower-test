f096044307a7a36d8d32c50c0da51e1cb61fb25b
ebtalley <eric@leadcliq.com>
2015-10-21 09:58:18 -0700

    pdf password protect

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -49,11 +49,18 @@ class HomeController < ApplicationController
 
   def download_pdf
 
-    send_file(
-        "#{Rails.root}/public/EPICERAM_L_ORDER_FORM_REV.pdf",
-        filename: "EPICERAM_L_ORDER_FORM_REV.pdf",
-        type: "application/pdf"
-    )
+    if physicians_physician_signed_in?
+
+      send_file(
+          "#{Rails.root}/public/EPICERAM_L_ORDER_FORM_REV.pdf",
+          filename: "EPICERAM_L_ORDER_FORM_REV.pdf",
+          type: "application/pdf"
+      )
+    else
+      redirect_to '/physicians/physicians/sign_in'
+      return
+    end
+
   end
 
 end
