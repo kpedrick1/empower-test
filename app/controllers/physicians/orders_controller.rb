7e2a8c6663dbdc1600510d06e7e108922bda34a0
ebtalley <eric@leadcliq.com>
2016-07-07 12:03:53 -0700

    shipping discount july 4th

--- app/controllers/physicians/orders_controller.rb
+++ app/controllers/physicians/orders_controller.rb
@@ -19,36 +19,11 @@ class Physicians::OrdersController < Physicians::ApplicationController
 
     session[:first_orders] = @first_orders
 
-    #get_first_order_account
-
-
-
-    puts "\n"
-    puts "\n"
-
-    puts @order_line
-
-    puts "\n"
-    puts "\n"
-
-    # @oppId = params[:oppId]
-    #
     @commit_action = session['commit']
     @payment_type = session['paymentType']
 
 
     @oppId = session['oppId']
-
-
-    puts '-----------------START @commit_action -------------------------'
-    puts "\n"
-    puts @commit_action
-    puts "\n"
-    puts '-----------------END @commit_action -------------------------'
-    puts "\n"
-
-    puts "\n"
-    puts "\n"
 
 
     if @commit_action == 'Place Order'
@@ -58,13 +33,6 @@ class Physicians::OrdersController < Physicians::ApplicationController
 
       shipping = session['shippingType']
 
-      puts "\n"
-      puts "--------START ProductLine---------\n"
-      puts "\n"
-
-
-      #puts "---------shipping----------------\n"
-      #puts shipping
 
       @order_line.each { |orderline|
         products.each { |product|
@@ -73,9 +41,6 @@ class Physicians::OrdersController < Physicians::ApplicationController
           end
 
 
-          puts "\n"
-          puts product['qty']
-          puts "\n"
 
           if product['qty'] == nil || product['qty'] == '0'
             next
@@ -105,22 +70,19 @@ class Physicians::OrdersController < Physicians::ApplicationController
 
           orderline.totalPrice = orderline.qty.to_f * orderline.unitPrice.to_f
 
-          puts "\n\nproduct Id \n"
-          puts orderline.productId
-          puts "\n"
-          puts "product Array \n"
-          puts session[:first_orders]
-          puts "\n"
-
-          puts "Product in array\n"
-          puts session[:first_orders].include?(orderline.productId)
-          puts "\n\n\n"
-
 
           if (product['qty'].to_i >= 4 && orderline.rXrequired == false)
 
-            orderline.totalPrice = orderline.totalPrice.to_f - (orderline.totalPrice.to_f * 0.25)
-            orderline.discountFormatted = '25%'
+
+            discount_amount = (orderline.qty.to_i / 4) * orderline.unitPrice.to_f
+
+            orderline.totalPrice = orderline.totalPrice.to_f - discount_amount.to_f
+
+
+            orderline.discountFormatted = ActionController::Base.helpers.number_to_currency(discount_amount)
+
+
+
 
           elsif (session[:first_orders].include?(orderline.productId) && orderline.rXrequired == false)
 
@@ -192,26 +154,11 @@ class Physicians::OrdersController < Physicians::ApplicationController
 
       }
 
-      puts @opp_lines
-
-      puts "\n"
-      puts "-------END ProductLine----------\n"
-      puts "\n"
-
 
     elsif @commit_action == 'Accept'
 
 
       products = session['productline']
-
-      puts "\n"
-      puts "--------START ProductLine---------\n"
-      puts "\n"
-
-
-      #puts "---------shipping----------------\n"
-      #puts shipping
-
 
 
       @order_line.each { |orderline|
@@ -249,10 +196,12 @@ class Physicians::OrdersController < Physicians::ApplicationController
 
           if (product['qty'].to_i >= 4 && orderline.rXrequired == false)
 
-            orderline.totalPrice = orderline.totalPrice.to_f - (orderline.totalPrice.to_f * 0.25)
-            orderline.discountFormatted = '25%'
+            discount_amount = (orderline.qty.to_i / 4) * orderline.unitPrice.to_f
+            orderline.totalPrice = orderline.totalPrice.to_f - discount_amount.to_f
+            orderline.discountFormatted = ActionController::Base.helpers.number_to_currency(discount_amount)
 
-          elsif (session[:first_orders].include? orderline.productId && orderline.rXrequired == false)
+
+          elsif (session[:first_orders].include?(orderline.productId) && orderline.rXrequired == false)
 
             orderline.totalPrice = orderline.totalPrice.to_f - (orderline.totalPrice.to_f * 0.20)
             orderline.discountFormatted = '20%'
@@ -279,8 +228,6 @@ class Physicians::OrdersController < Physicians::ApplicationController
           if product['qty'] == nil
             next
           end
-
-          #puts "#{product}\n"
 
           shippingopt.qty = 1
 
@@ -323,11 +270,6 @@ class Physicians::OrdersController < Physicians::ApplicationController
 
     delete_cart_session
 
-    puts "-------------first_orders ------------------ \n"
-
-    puts session[:first_orders]
-
-
   end
 
   def has_multi_dist
@@ -340,30 +282,8 @@ class Physicians::OrdersController < Physicians::ApplicationController
 
   end
 
-  # def get_first_order_account
-  #
-  #   account_id = session[:account_id]
-  #
-  #   client = Restforce.new
-  #
-  #   accounts = client.query("select Id, Orders__c from Account where Id = '#{account_id}'")
-  #
-  #   account = accounts.first
-  #
-  #
-  #   if (!account.nil? && account.Orders__c > 0)
-  #     session[:has_order] = true
-  #   else
-  #     session[:has_order] = false
-  #   end
-  #
-  #   @has_order = session[:has_order]
-  #
-  #
-  # end
 
   def get_opportunity_salesforce
-
 
     account_id = session[:account_id]
 
@@ -373,7 +293,6 @@ class Physicians::OrdersController < Physicians::ApplicationController
 
 
     @opportunity = result.body.opportunity
-
 
 
   end
@@ -400,29 +319,9 @@ class Physicians::OrdersController < Physicians::ApplicationController
 
   def create
 
-      #send data to salesforce endpoint
-
-    puts "\n"
-    puts "\n"
-    puts '-----------------create called -------------------------'
-    puts "--------start params.inspect--------\n"
-    puts "\n"
-    puts params.inspect
-    puts "\n"
-    puts "------- end params.inspect --------\n"
-    puts "\n"
-    puts 'commit::::::::'
-    puts "\n"
-    puts params['commit']
-    puts "\n"
-    puts "\n"
-
     create_cart_session
 
     if params['commit'] == 'Accept'
-
-
-      puts 'place order called'
 
 
       client = Restforce.new
@@ -432,16 +331,6 @@ class Physicians::OrdersController < Physicians::ApplicationController
       #args["order"] = params[:inventory]
 
       result = client.post '/services/apexrest/portal/order_product/', :userid => session[:account_id], :par => params
-
-
-      puts "\n"
-      puts "\n"
-      puts '-----------------result -------------------------'
-      puts result
-      puts "\n"
-      puts result.body
-      puts "\n"
-      puts "\n"
 
       session['oppId'] = result.body['opportunityId']
 
@@ -453,7 +342,7 @@ class Physicians::OrdersController < Physicians::ApplicationController
       return
     end
 
-    redirect_to action: "index"
+    redirect_to :action => 'index'
 
   end
 
