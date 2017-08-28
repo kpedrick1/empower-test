7e8277b4ee245f8a349992923c35cdf0170e8434
Kelly Pedrick <pedrick@symplmed.com>
2017-08-28 11:06:18 -0700

    shipping fix

--- app/controllers/patients/checkout_controller.rb
+++ app/controllers/patients/checkout_controller.rb
@@ -164,7 +164,7 @@ class Patients::CheckoutController < ApplicationController
 
     shipping_book.each do |ship_item|
 
-      if has_eptx == true
+      if has_eptex == true
 
         if ship_item.productCode == 'Shipping Eptex'
 
