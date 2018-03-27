1be71b8a3a6c49abc9ecaaafbe5bedde100e79c9
Kelly Pedrick <pedrick@symplmed.com>
2018-03-27 11:18:27 -0700

    shipping update

--- app/controllers/patients/checkout_controller.rb
+++ app/controllers/patients/checkout_controller.rb
@@ -180,21 +180,21 @@ class Patients::CheckoutController < ApplicationController
           break
         end
 
-      # else
+      else
 
-      #   if ship_item.productCode == 'ShippingEnl'
+        if ship_item.productCode == 'ShippingEnl'
 
-      #     ship_item.quantity = 1
-      #     ship_item.totalPrice = ship_item.productPrice
+          ship_item.quantity = 1
+          ship_item.totalPrice = ship_item.productPrice
 
-      #     @cart_grand_total += ship_item.productPrice
+          @cart_grand_total += ship_item.productPrice
 
-      #     @cart_items.push(ship_item)
+          @cart_items.push(ship_item)
 
-      #     break
-      #   end
+          break
+        end
 
-      # end
+      end
 
     end
 
